6df64056f84396ee7c9a87dbe03bd22b82e4231b
Matheus Henrique <henriquematheusg3@gmail.com>
2020-12-08 11:34:03 -0300

    firts commit

---
+++ src/routes/appointments.routes.ts
@@ -0,0 +1,33 @@
+import { Router } from 'express';
+
+import { startOfHour, parseISO, isEqual } from 'date-fns';
+
+import Appointment from '../models/Appointment';
+
+const appointmentRouter = Router();
+
+const appointments: Appointment[] = [];
+
+appointmentRouter.post('/', (request, response) => {
+  const { provider, date } = request.body;
+
+  const parsedDate = startOfHour(parseISO(date));
+
+  const findAppointmentInSameDate = appointments.find((appointment) => isEqual(
+    parsedDate, appointment.date,
+  ));
+
+  if (findAppointmentInSameDate) {
+    return response
+      .status(400)
+      .json({ message: 'This appointment is already booked' });
+  }
+
+  const appointment = new Appointment(provider, parsedDate);
+
+  appointments.push(appointment);
+
+  return response.json(appointment);
+});
+
+export default appointmentRouter;
